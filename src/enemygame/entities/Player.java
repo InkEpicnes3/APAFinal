95b9df5fbcbb415a6caf3bf18164f72f52b096c6
Ink <inkepicnes3@gmail.com>
2024-05-01 21:41:52 -0400

    Get player to move

--- src/enemygame/entities/Player.java
+++ src/enemygame/entities/Player.java
@@ -3,22 +3,56 @@ package enemygame.entities;
 import enemygame.logic.Vector;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class Player extends Entity {
+public class Player extends Entity implements KeyListener {
+    private final ArrayList<Integer> pressedKeys;
 
+    public Player(Point position, Dimension size, int speed) {
+        super(position, size, speed);
+        pressedKeys = new ArrayList<>();
+    }
 
     @Override
     public void tick() {
-        Vector inputDir = new Vector();
+        velocity = new Vector();
+        if (pressedKeys.contains(KeyEvent.VK_W))
+            velocity.add(new Vector(0, -1));
+        if (pressedKeys.contains(KeyEvent.VK_S))
+            velocity.add(new Vector(0, 1));
+        if (pressedKeys.contains(KeyEvent.VK_A))
+            velocity.add(new Vector(-1, 0));
+        if (pressedKeys.contains(KeyEvent.VK_D))
+            velocity.add(new Vector(1, 0));
+        velocity.setLength(speed);
+        move();
     }
 
     @Override
     public void draw(Graphics g) {
-
+        g.setColor(Color.MAGENTA);
+        g.fillRect((int) position.getX(), (int) position.getY(), (int) size.getWidth(), (int) size.getHeight());
     }
 
     @Override
     public Rectangle getCollision() {
-        return null;
+        return new Rectangle(position, size);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (!pressedKeys.contains(e.getKeyCode()))
+            pressedKeys.add(e.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        pressedKeys.remove((Integer) e.getKeyCode());
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 }
--- src/enemygame/logic/Vector.java
+++ src/enemygame/logic/Vector.java
@@ -1,7 +1,8 @@
 package enemygame.logic;
 
 public class Vector {
-    private double x, y;
+    private double x;
+    private double y;
 
     public Vector(double x, double y) {
         this.x = x;
@@ -15,5 +16,33 @@ public class Vector {
     public void add(Vector v) {
         this.x += v.x;
         this.y += v.y;
+    }
+
+    public void setLength(double newLength) {
+        normalize();
+        x *= newLength;
+        y *= newLength;
+    }
+
+    public void normalize() {
+        double totalLength = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        x /= totalLength;
+        y /= totalLength;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
     }
 }
