d7c03e6dbbd84e2e5974fd342e31cf9bbfab803a
Ink <inkepicnes3@gmail.com>
2024-05-03 21:26:29 -0400

    Implement enemy spawning / damage + projectile firing

--- src/enemygame/EnemyGame.java
+++ src/enemygame/EnemyGame.java
@@ -1,24 +1,33 @@
 package enemygame;
 
+import enemygame.entities.Enemy;
 import enemygame.entities.Player;
-import enemygame.logic.GameTickManager;
+import enemygame.entities.Projectile;
+import enemygame.logic.EnemyManager;
+import enemygame.logic.InputManager;
+import enemygame.logic.ProjectileManager;
+import enemygame.util.DoublePoint;
+import enemygame.util.Vector;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class EnemyGame implements Runnable {
     private static JFrame window;
     private static EnemyGamePanel gamePanel;
-    private static GameTickManager tickManager;
+
+    private static InputManager input;
+    private static EnemyManager enemyManager;
+    private static ProjectileManager projectileManager;
 
     private static Player player;
-
-    private final boolean gameQuit;
     private final Thread thread;
+    private boolean gameQuit;
 
     public EnemyGame() {
         window = new JFrame("Enemy Game");
-        window.setSize(new Dimension(1366, 768));
+        window.setPreferredSize(new Dimension(1920, 1080));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setUndecorated(true);
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -29,12 +38,13 @@ public class EnemyGame implements Runnable {
 
         gameQuit = false;
         thread = new Thread(this);
-        tickManager = new GameTickManager();
+        input = new InputManager(window);
 
-        player = new Player(new Point(500, 500), new Dimension(100, 100), 10);
-        tickManager.add(player);
-        gamePanel.addDrawable(player);
-        window.addKeyListener(player);
+        enemyManager = new EnemyManager(window.getPreferredSize(), 300);
+
+        projectileManager = new ProjectileManager();
+
+        player = new Player(new DoublePoint(500, 500), new Dimension(100, 100), 10);
 
         thread.start();
         window.setVisible(true);
@@ -43,8 +53,23 @@ public class EnemyGame implements Runnable {
     @Override
     public void run() {
         while (!gameQuit) {
-            tickManager.tick();
+            enemyManager.trySpawnEnemy();
+            enemyManager.tick();
+            projectileManager.tick();
+            player.tick();
             gamePanel.repaint();
+
+            for (Enemy e : enemyManager.getEnemies())
+                for (Projectile p : projectileManager.getProjectiles())
+                    if (p.collidesWith(e)) {
+                        e.getVelocity().add(new Vector(p.getVelocity().getX() * 5, p.getVelocity().getY() * 5));
+                        e.move();
+                        e.damage(p.getDamage());
+                        projectileManager.queueForRemoval(p);
+                    }
+
+            if (input.isKeyPressed(KeyEvent.VK_F8))
+                gameQuit = true;
 
             try {
                 Thread.sleep(16);
@@ -62,12 +87,20 @@ public class EnemyGame implements Runnable {
         return gamePanel;
     }
 
+    public static InputManager getInput() {
+        return input;
+    }
+
     public static Player getPlayer() {
         return player;
     }
 
-    public static GameTickManager getTickManager() {
-        return tickManager;
+    public static EnemyManager getEnemyManager() {
+        return enemyManager;
+    }
+
+    public static ProjectileManager getProjectileManager() {
+        return projectileManager;
     }
 
     public static void main(String[] args) {
